d4c7431681f90aa252e23c36574d40e016b2076f
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-24 00:45:11 +0530

    merge_refresh

--- utils/xgettest_test.bat
+++
@@ -1,9 +0,0 @@
-
-pushd C:\xampp\htdocs\php-addressbook\translations
-
-%~dp0gettext\bin\msgfmt.exe php-addressbook-no.po -o LOCALES\no\LC_MESSAGES\php-addressbook.mo
-%~dp0gettext\bin\msgfmt.exe php-addressbook-ua.po -o LOCALES\ua\LC_MESSAGES\php-addressbook.mo
-
-popd
-
-pause
